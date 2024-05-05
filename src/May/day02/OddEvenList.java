79172d131dd8b5541113c5dd7cf186ca9dbc76f0
ComeOnWang <1879519397@qq.com>
2024-05-05 15:05:44 +0800

    ai 帮我做题

---
+++ src/May/day02/OddEvenList.java
@@ -0,0 +1,50 @@
+package May.day02;
+
+/**
+ * Author:   wang
+ * Date:     2024/5/5 14:53
+ * function:
+ */
+public class OddEvenList {
+    /**
+     * 时间复杂的：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
