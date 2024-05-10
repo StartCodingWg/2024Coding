4de426d8b1d094f2c0e0c15a965704901ac937c7
wanggh31690 <1879519397@qq.com>
2024-05-10 20:12:54 +0800

    轻轻松松

---
+++ src/May/day09/LowestCommonAncestor.java
@@ -0,0 +1,40 @@
+package May.day09;
+
+import CommonClass.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+
+/**
+ * 时间：2024/5/9
+ * 问题描述：给定一个二叉树, 找到该树中两个指定节点的最近公共祖先
+ * 切入点/解决思路：
+ * 感想：
+ */
+public class LowestCommonAncestor {
+    private TreeNode result;
+
+    /**
+     * 使用深度优先搜索
+     * 时间复杂度：O(n)
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        helper(root, p, q);
+        return result;
+    }
+
+    public boolean helper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean left = helper(root.left, p, q);
+        boolean right = helper(root.right, p, q);
+        if ((left && right) || (left || right) && (root == p || root == q)) {
+            result = root;
+            return false;
+        }
+        return left || right || (root == p || root == q);
+    }
+}
